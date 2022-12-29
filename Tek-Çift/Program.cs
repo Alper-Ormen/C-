38b5f120758f94c6be8a6bb138bf320533d69926
Alper Örmen <119490077+Alper-Ormen@users.noreply.github.com>
2022-12-29 23:31:36 +0300

    Tek Çift

---
+++ Tek-Çift/Program.cs
@@ -0,0 +1,13 @@
+﻿Random rnd = new Random();
+
+int sayi = rnd.Next(100);
+
+if (sayi % 2 == 0)
+{
+
+    System.Console.Write("{0} çift sayıdır", sayi);
+}
+else
+{
+    System.Console.WriteLine("{0} tek sayıdır", sayi);
+}
