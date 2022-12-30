645e4c5bccd1bcf2db8ddaefc030fd97a5a739df
Alper Örmen <119490077+Alper-Ormen@users.noreply.github.com>
2022-12-31 00:02:21 +0300

    C# TodoList DB

---
+++ todolistc/Models/Liste.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace c_.Models;
+
+public partial class Liste
+{
+    public string? Adi { get; set; }
+
+    public string? Soyadi { get; set; }
+
+    public DateOnly? Tarih { get; set; }
+}
